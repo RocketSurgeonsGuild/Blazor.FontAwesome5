3bf17931c11312eb0a5942a94585e018d28ce568
Rocket Understudy <33589210+rsg-bot@users.noreply.github.com>
2024-09-04 18:24:26 -0400

    Automatically linting code

--- .build/Build.CI.cs
+++ .build/Build.CI.cs
@@ -1,5 +1,5 @@
+using System.Diagnostics;
 using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.CI.GitHubActions.Configuration;
 using Rocket.Surgery.Nuke.ContinuousIntegration;
 using Rocket.Surgery.Nuke.DotNetCore;
 using Rocket.Surgery.Nuke.GithubActions;
@@ -10,21 +10,21 @@ using Rocket.Surgery.Nuke.GithubActions;
     "ci-ignore",
     GitHubActionsImage.UbuntuLatest,
     AutoGenerate = false,
-    On = [RocketSurgeonGitHubActionsTrigger.Push],
-    OnPushTags = ["v*"],
-    OnPushBranches = ["master", "main", "next"],
-    OnPullRequestBranches = ["master", "main", "next"],
-    Enhancements = [nameof(CiIgnoreMiddleware)]
+    On = [RocketSurgeonGitHubActionsTrigger.Push,],
+    OnPushTags = ["v*",],
+    OnPushBranches = ["master", "main", "next",],
+    OnPullRequestBranches = ["master", "main", "next",],
+    Enhancements = [nameof(CiIgnoreMiddleware),]
 )]
 [GitHubActionsSteps(
     "ci",
     GitHubActionsImage.UbuntuLatest,
     AutoGenerate = false,
-    On = [RocketSurgeonGitHubActionsTrigger.Push],
-    OnPushTags = ["v*"],
-    OnPushBranches = ["master", "main", "next"],
-    OnPullRequestBranches = ["master", "main", "next"],
-    InvokedTargets = [nameof(Default)],
+    On = [RocketSurgeonGitHubActionsTrigger.Push,],
+    OnPushTags = ["v*",],
+    OnPushBranches = ["master", "main", "next",],
+    OnPullRequestBranches = ["master", "main", "next",],
+    InvokedTargets = [nameof(Default),],
     NonEntryTargets =
     [
         nameof(ICIEnvironment.CIEnvironment),
@@ -33,43 +33,34 @@ using Rocket.Surgery.Nuke.GithubActions;
         nameof(IGenerateCodeCoverageBadges.GenerateCodeCoverageBadges),
         nameof(IGenerateCodeCoverageReport.GenerateCodeCoverageReport),
         nameof(IGenerateCodeCoverageSummary.GenerateCodeCoverageSummary),
-        nameof(Default)
+        nameof(Default),
     ],
-    ExcludedTargets = [nameof(ICanClean.Clean), nameof(ICanRestoreWithDotNetCore.DotnetToolRestore)],
-    Enhancements = [nameof(CiMiddleware)]
+    ExcludedTargets = [nameof(ICanClean.Clean), nameof(ICanRestoreWithDotNetCore.DotnetToolRestore),],
+    Enhancements = [nameof(CiMiddleware),]
 )]
 [GitHubActionsLint(
     "lint",
     GitHubActionsImage.UbuntuLatest,
     AutoGenerate = false,
-    OnPullRequestTargetBranches = ["master", "main", "next"],
-    Enhancements = [nameof(LintStagedMiddleware)]
+    OnPullRequestTargetBranches = ["master", "main", "next",],
+    Enhancements = [nameof(LintStagedMiddleware),]
 )]
 [PrintBuildVersion]
 [PrintCIEnvironment]
 [UploadLogs]
 [TitleEvents]
 [ContinuousIntegrationConventions]
-[System.Diagnostics.DebuggerDisplay("{DebuggerDisplay,nq}")]
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
 public partial class Pipeline
 {
-    [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
-    private string DebuggerDisplay
-    {
-        get
-        {
-            return ToString();
-        }
-    }
-
     public static RocketSurgeonGitHubActionsConfiguration CiIgnoreMiddleware(RocketSurgeonGitHubActionsConfiguration configuration)
     {
         ( (RocketSurgeonsGithubActionsJob)configuration.Jobs[0] ).Steps =
         [
             new RunStep("N/A")
             {
-                Run = "echo \"No build required\""
-            }
+                Run = "echo \"No build required\"",
+            },
         ];
 
         return configuration.IncludeRepositoryConfigurationFiles();
@@ -83,7 +74,7 @@ public partial class Pipeline
            .Jobs.OfType<RocketSurgeonsGithubActionsJob>()
            .First(z => z.Name.Equals("build", StringComparison.OrdinalIgnoreCase))
            .UseDotNetSdks("8.0")
-           // .ConfigureForGitVersion()
+            // .ConfigureForGitVersion()
            .ConfigureStep<CheckoutStep>(step => step.FetchDepth = 0)
            .PublishLogs<Pipeline>();
 
@@ -99,4 +90,7 @@ public partial class Pipeline
 
         return configuration;
     }
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private string DebuggerDisplay => ToString();
 }

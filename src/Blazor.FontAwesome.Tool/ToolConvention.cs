2cde15ef4af5d037a6b61eace30e88425529260d
David Driscoll <david.driscoll@gmail.com>
2025-02-24 20:10:39 -0500

    Updated to new msbuild package

--- src/Blazor.FontAwesome.Tool/ToolConvention.cs
+++ src/Blazor.FontAwesome.Tool/ToolConvention.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 using Rocket.Surgery.Blazor.FontAwesome.Tool.Commands;
 using Rocket.Surgery.Blazor.FontAwesome.Tool.Support;
 using Rocket.Surgery.Conventions;
 using Rocket.Surgery.Conventions.CommandLine;
 using Rocket.Surgery.Conventions.DependencyInjection;
+
 using Spectre.Console.Cli;
 
 namespace Rocket.Surgery.Blazor.FontAwesome.Tool;
@@ -12,21 +14,15 @@ namespace Rocket.Surgery.Blazor.FontAwesome.Tool;
 [ExportConvention]
 internal class ToolConvention : ICommandLineConvention, IServiceConvention
 {
-    public void Register(IConventionContext context, IConfigurator app)
-    {
-        app.AddCommand<EmitCommand>("emit");
-    }
+    public void Register(IConventionContext context, IConfigurator app) => app.AddCommand<EmitCommand>("emit");
 
-    public void Register(IConventionContext context, IConfiguration configuration, IServiceCollection services)
-    {
-        services
-           .AddTransient<CategoryProvider>(_ => CategoryProvider.Instance ?? CategoryProvider.CreateDefault())
-           .AddSingleton<FontAwesomeApiKeyProvider>()
-           .AddSingleton<FontAwesomeApiKeyHandler>()
-           .AddFontAwesome()
-           .ConfigureHttpClient(
-                client => client.BaseAddress = new("https://api.fontawesome.com/"),
-                builder => builder.AddHttpMessageHandler<FontAwesomeApiKeyHandler>()
-            );
-    }
+    public void Register(IConventionContext context, IConfiguration configuration, IServiceCollection services) => services
+        .AddTransient(_ => CategoryProvider.Instance ?? CategoryProvider.CreateDefault())
+        .AddSingleton<FontAwesomeApiKeyProvider>()
+        .AddSingleton<FontAwesomeApiKeyHandler>()
+        .AddFontAwesome()
+        .ConfigureHttpClient(
+             client => client.BaseAddress = new("https://api.fontawesome.com/"),
+             builder => builder.AddHttpMessageHandler<FontAwesomeApiKeyHandler>()
+         );
 }
